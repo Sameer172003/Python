677cd8cf95a9ca2cb37d019290e2f5d83648d233
Sameer Ojha <146799637+Sameer172003@users.noreply.github.com>
2025-06-26 11:23:21 +0530

    added

---
+++ tut1.py
@@ -0,0 +1,19 @@
+# Chapter 1 – Modules, Comments & pip
+
+print("Hello World")
+
+# Modules - A module is a file containing code written by somebody else which can be imported and used in our program.
+
+# Pip - Pip is the package manager for python you can use pip to install a module on your system.
+
+# Example - pip install flask ---> it install flask module in the system
+
+# Types of modules - 1) Built in modules 2) External modules
+
+# Comments - Comments are used to write something which the programmer does not want to execute.
+
+# Types of comments - 1) Single line (#) 2) Multi line ("""......""")
+
+
+
+
