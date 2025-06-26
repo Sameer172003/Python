4ffa6a7afa86a84f244d53129e07e7ee5b934742
Sameer Ojha <146799637+Sameer172003@users.noreply.github.com>
2025-06-26 12:29:57 +0530

    added

---
+++ tut2..py
@@ -0,0 +1,48 @@
+# Chapter 2 – Variables and Datatype
+
+# Variable - It is a container that store a value
+# Keywords - Reserved words in Python
+# Identifiers - Class/ Function/ Variable name
+
+# Datatypes -
+# Types - 1) Integers 2) Strings 3) Boolean 4) Floating Point Number 5) None
+
+# Operators -
+# Types - 1) Arithmatic (+,-,*,/) 2) Assignment (=,+=,-=,*=,/=) 3) Comparision (==,>=,<=,!=) 4) Logical (and,or,not)
+
+a=1
+b=2
+print(a+b)
+
+# type() - used to find the datatype of a given variable
+
+print(type(a))
+
+c="sameer"
+
+print(type(c))
+
+# Typecast
+
+# 1) Integer to String
+
+str(31)
+
+# 2) String to Integer
+
+int("32")
+
+# how to take input
+
+a1=int(input("Enter number 1:"))
+a2=int(input("Enter number 2:"))
+sum=a1+a2
+print(sum)
+
+# Write a python program to find the avg of two numbers
+
+n1=int(input("Enter num 1 :"))
+n2=int(input("Enter num 2 :"))
+avg=(n1+n2)/2
+print(avg)
+
